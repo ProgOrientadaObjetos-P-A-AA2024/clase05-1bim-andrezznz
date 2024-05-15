fc751815dfbfabaf9bd1d1a6c3bfe4014528a425
Andres Cardenas <ascardenas5@utpl.edu.ec>
2024-05-15 16:14:21 -0500

    Comentarios Dentro del Ejercicio en Clase

--- EjemplosArreglos/EjercicioArreglosPOO/src/paquete02/Ejecutor2.java
+++ EjemplosArreglos/EjercicioArreglosPOO/src/paquete02/Ejecutor2.java
@@ -1,21 +1,32 @@
 package paquete02;
 
+// Realizado Por Andres Cardenas
 import paquete01.LibretaCalificacion;
 
 public class Ejecutor2 {
 
     public static void main(String[] args) {
-        
-        double [] notas = {10, 9, 8, 10};
-        
+
+        double[] notas = {10, 9, 8, 10};
+
         String nombreEstudiante = " Rene Elizalde ";
-        
-        LibretaCalificacion libreta  = new LibretaCalificacion(nombreEstudiante,
+
+        LibretaCalificacion libreta = new LibretaCalificacion(nombreEstudiante,
                 notas);
-        
+
         libreta.establecerPromedio();
-        libreta.establecerPromedioCualitativo();
-        System.out.printf("\n%s\n", libreta);
+        /* Si se lo muestra como comentado al metodo establecerPromedio
+        no se envia las notas, por lo tanto no calcula el promedio y por defecto 
+        nos muestra en pantalla 0
+         */
         
-    }  
+        libreta.establecerPromedioCualitativo();
+        /* Si se lo muestra como comentado al metodo establecerPromedioCualitativo
+        no se envia el promedio del metodo anterior, por lo tanto nos muestra 
+        un Mensaje Null
+         */
+        
+        System.out.printf("\n%s\n", libreta);
+
+    }
 }
